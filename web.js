47ad56d5137fba472c3874be86696914132d5a8f
Carlo Cosenza <boredagainpiston@boredagainpiston-desktop.(none)>
2013-07-12 23:44:45 -0700

    updated web.js.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var fs = require('fs');
+  var buf = fs.readFileSync('index.html','utf-8');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
